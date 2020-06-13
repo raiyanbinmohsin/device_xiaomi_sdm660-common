44ad4b4a8f1404476fd6ce48010d98cdef838467
Jesse Chan <jc@lineageos.org>
2020-06-13 22:45:25 +0800

    sdm660-common: enable unspecialized app process pool
    Change-Id: I6dfcb59dace5eaa7551caeae86db3b485c0cfbf1

--- properties.mk
+++ properties.mk
@@ -200,3 +200,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # WFD
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.debug.wfd.enable=1
+
+# Zygote
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
