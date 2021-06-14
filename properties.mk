bc069b2c1a7d136db60123e10a9c85bed9d68f46
raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>
2021-06-15 04:30:56 +0600

    sdm660-common: prop: Enable UI blur by default

--- properties.mk
+++ properties.mk
@@ -88,6 +88,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.bluetooth.soc=cherokee
+    
+# Blur
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1 \
+    debug.sf.disable_backpressure=1
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
