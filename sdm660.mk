0a319abe451979fd0c81c1d6fd9e3fe98ecb0dba
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-13 00:02:51 +0200

    sdm660-common: Remove vndk-ext
    * It was only useful to jason, which needs a copy of libpowermanager in vendor
    
    Change-Id: I754c666730d841fd71837358fe87a5ce1a3d7921
    Signed-off-by: raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -397,10 +397,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
-# VNDK
-PRODUCT_PACKAGES += \
-    vndk-ext
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
